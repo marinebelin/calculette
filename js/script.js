94fe74145947fdfea00d20ca9e194d801593ff11
cecile <cec.jobert@gmail.com>
2017-05-05 10:10:50 +0200

    correction js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,41 @@
 var lestouches=document.querySelectorAll(".touche");
 var ecran=document.getElementById("ecran");
 var egale=document.querySelector(".egal");
-
+var ce=document.querySelector(".reinit");
 
 for(i=0; i<lestouches.length;i++){
     lestouches[i].addEventListener("click",calcul);
-
+    
 }
 
 egale.addEventListener("click",somme);
-
+ce.addEventListener("click",reinitialiser);
 
 function calcul(){
     var recup = this.innerHTML;
-   ecran.innerHTML+=recup;
+    ecran.innerHTML+=recup;
+  
+    
    
 }
 
 function somme(){
     var result=eval(ecran.innerText);
-    ecran.innerHTML=ecran.innerHTML+" = "+result;
+    var final = Math.round(result*100)/100;
+    ecran.innerHTML=ecran.innerHTML+" = "+final;
+    for(i=0; i<lestouches.length;i++){
+        lestouches[i].removeEventListener("click",calcul);
+    }
+      
 }
+
+function reinitialiser(){
+    ecran.innerHTML="";
+    for(i=0; i<lestouches.length;i++){
+    lestouches[i].addEventListener("click",calcul);
+    
+}
+
+    
+}
+//Math.round(ton_chiffre*100)/100;  arrondir a deux chiffres apres la virgule
